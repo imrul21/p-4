de99f7ae265399fc24bb42de55384b5d3224fa01
imrul21 <114300430+imrul21@users.noreply.github.com>
2023-05-14 16:37:21 +0600

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+$(function () {
+
+ $(".banner_slider").slick({
+    arrows: false,
+    fade: true,
+    speed: 500,
+    autoplay: true,
+    dots: true,
+    dotsClass: 'banner_slider_dots'
+  
+ });
+
+
+
+});
+
+
+
+
+
+
+
+
+
